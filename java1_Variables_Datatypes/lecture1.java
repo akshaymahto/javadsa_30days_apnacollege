dc2470f2ce9c5f11781db6958057745291bad3e1
akshaymahto <akshay.mahto17101999@gmail.com>
2026-02-11 00:49:08 -0600

    Initial commit: Java DSA 30 days course

---
+++ java1_Variables_Datatypes/lecture1.java
@@ -0,0 +1,93 @@
+
+// // To print Hello World 
+// public class lecture1 {
+//     // Output print ln is use to print next line
+
+//     public static void main(String[] args){
+//         System.out.println("Hello world"); // Hello world
+//         System.out.println("Hello world"); // Hello world
+//     }
+    
+// }
+
+// PRINT PATTERN
+// *
+// **
+// ** *
+// ** **
+
+// public class lecture1 {
+//     public static void main(String[] args) {
+//         // write your code here
+//         System.out.println("*");
+//         System.out.println("**");
+//         System.out.println("***");
+//         System.out.println("****");
+//     }
+// }
+
+// // VARIABLES IN JAVA
+
+// public class lecture1 {
+// public static void main(String[] args) {
+// // Variables is Java
+// int a = 25;
+// int b = 25;
+// int sum = a + b;
+// int sub = a - b;
+// int mul = a * b;
+// int div = a / b;
+// System.out.println(sum); // 50
+// System.out.println(sub); // 0
+// System.out.println(mul); // 625
+// System.out.println(div); // 1
+
+// }
+// }
+
+// INPUT FROM USER
+
+// import java.util.*;
+
+// public class lecture1 {
+// public static void main(String[] args) {
+// Scanner sc = new Scanner(System.in);
+// // String name = sc.next();// next() print single world
+// String name = sc.nextLine();// next() print multiple words
+// // nextInt()
+// // nextFloat()
+// System.out.println(name);
+// }
+// }
+
+// Q. Take 2 input from user and print Sum of them
+
+// import java.util.Scanner;
+
+// public class lecture1 {
+
+// public static void main(String[] args) {
+// Scanner sc = new Scanner(System.in);
+// System.out.print("Enter number 1:");
+// int a = sc.nextInt();
+// System.out.print("Enter Number 2:");
+// int b = sc.nextInt();
+// int add = a + b;
+// System.out.print("sum of two numbers :");
+// System.out.println(add);
+// }
+// }
+
+// // Q . Find Area of Square
+// import java.util.*;
+
+// public class lecture1 {
+// public static void main(String[] args) {
+// Scanner side = new Scanner(System.in);
+// System.out.print("Enter One Side Of Square:");
+// int a = side.nextInt();
+// int Area = (a * a);
+// System.out.print("Area of Square is :");
+// System.out.println(Area);
+// }
+// }
