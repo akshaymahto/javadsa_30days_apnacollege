f4203baeb804741b5b8c2b4f6cb1bb7d532ae14b
akshaymahto <akshay.mahto17101999@gmail.com>
2026-06-27 02:07:17 -0500

    notes updated

--- java19_Recursion_III/Recursion3.java
+++ java19_Recursion_III/Recursion3.java
@@ -112,24 +112,24 @@ import java.sql.Time;
 
 // second Method
 
-public class Recursion3 {
-    public static int callGuests(int n){
-        if(n<=1){
-            return 1;
-        }
-        //single
-        int ways1 = callGuests(n-1);
+// public class Recursion3 {
+//     public static int callGuests(int n){
+//         if(n<=1){
+//             return 1;
+//         }
+//         //single
+//         int ways1 = callGuests(n-1);
 
-        //pair
-        int ways2 = (n-1) * callGuests(n-2);
-        return ways1 + ways2;
+//         //pair
+//         int ways2 = (n-1) * callGuests(n-2);
+//         return ways1 + ways2;
 
-    }
-    public static void main(String[] args) {
-        int n = 4;
-        System.out.println(callGuests(n));
-    }
-}
+//     }
+//     public static void main(String[] args) {
+//         int n = 4;
+//         System.out.println(callGuests(n));
+//     }
+// }
 
 
 //----------------------------------------------------------
